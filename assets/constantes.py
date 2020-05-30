64110aa842537511847cefa91467966705c11726
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2020-05-30 21:37:01 +0100

    Create constantes.py

---
+++ assets/constantes.py
@@ -0,0 +1,3 @@
+ncromo = {1, 2, 5, 11, 13, 14, 15, 16, 19, 20, 21, 22}
+caracteristicas = ['Alzheimer', 'Cancro Colorretal', 'Asma', 'Albinismo', 'Cancro de Mama', 'Alzheimer', 'Albinismo e Olhos Castanhos/Azuis', 'Cancro Colorretal e Cerrume e Pigmentação da pele', 'Alzheimer e Olhos Verdes', 'Altura', 'Alzheimer', 'Transtorno Bipolar']
+yes = ('yes','y','sim','s')
